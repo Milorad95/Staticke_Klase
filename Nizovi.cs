0894c010cdbdbd9de610b38a4ab9d1a83ff637d9
Milorad Komljenovic <Miloradk95@gmail.com>
2021-12-12 17:25:10 +0100

    Add project files.

---
+++ Nizovi.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staticke_klase
+{
+    static class Nizovi
+    {
+        public static int[] Niz = new int[0];
+        public static int Brojac { get; set; }
+        public static int Zbir { get; set; }
+
+        // generisi niz duzine koja je prosledjena kao parametar
+        public static void generisi(int duzina)
+        {
+            Random rnd = new Random();
+            Niz = new int[duzina];
+
+            for (int i = 0; i < Niz.Length; i++)
+            {
+                Niz[i] = rnd.Next(0, 50);
+            }
+        }
+
+        // ispis clanova niza
+        public static void ispis()
+        {
+            foreach(var clan in Niz)
+            {
+                Console.Write($"{clan} ");
+            }
+        }
+
+        // ispis parnih ili neparnih clanova niza u zavisnosti koji je tip parnosti prosledjen kao parametar 
+        public static void ispisParnosti(Parnost parnost)
+        {
+            if(parnost == 0)
+            {
+                for (int i = 0; i < Niz.Length; i++)
+                {
+                    if(Niz[i] % 2 == 0)
+                    {
+                        Console.Write($"{Niz[i]} ");
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Niz.Length; i++)
+                {
+                    if (Niz[i] % 2 != 0)
+                    {
+                        Console.Write($"{Niz[i]} ");
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        // prosek zbira svih clanova niza
+        public static int prosek()
+        {
+            foreach(var broj in Niz)
+            {
+                Zbir += broj;
+                Brojac++;
+            }
+
+            return Zbir / Brojac;
+        }
+
+        // prosek parnih ili neparnih clanova niza u zavisnosti koji je tip parnosti prosledjen kao parametar
+        public static int prosekParnosti(Parnost parametar)
+        {      
+            if(parametar == 0)
+            {
+                foreach (var clan in Niz)
+                {
+                    if(clan % 2 == 0)
+                    {
+                        Zbir += clan;
+                        Brojac++;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var clan in Niz)
+                {
+                    if (clan % 2 != 0)
+                    {
+                        Zbir += clan;
+                        Brojac++;
+                    }
+                }
+            }
+
+            return Zbir / Brojac;
+        }
+
+        public enum Parnost
+        {
+            Parno,
+            Neparno
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Staticke_klase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nizovi.generisi(15);
+            Console.WriteLine("Ispis clanova niza: ");
+            Nizovi.ispis();
+
+            Console.ReadLine();
+
+            Console.WriteLine("Ispis parnih clanova niza: ");
+            Nizovi.ispisParnosti(Nizovi.Parnost.Parno);
+            Console.ReadLine();
+
+            Console.WriteLine("Ispis neparnih clanova niza: ");
+            Nizovi.ispisParnosti(Nizovi.Parnost.Neparno);
+            Console.ReadLine();
+
+            Nizovi.prosek();
+            Console.ReadLine();
+
+            Console.WriteLine("Prosek parnih clanova niza: ");
+            Console.WriteLine(Nizovi.prosekParnosti(Nizovi.Parnost.Parno));
+            Console.ReadLine();
+
+            Console.WriteLine("Prosek neparnih clanova niza: ");
+            Console.WriteLine(Nizovi.prosekParnosti(Nizovi.Parnost.Neparno));
+
+            Console.ReadLine();
+        }
+    }
+}
